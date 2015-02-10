3bbe05be68838fa04550aad5c31a34717898003e
Valc <rafaelgomezcunill@gmail.com>
2015-02-10 01:07:53 +0100

    Delete it from legend

--- component/admin/models/fields/legend.php
+++ component/admin/models/fields/legend.php
@@ -36,8 +36,6 @@ class JFormFieldLegend extends JFormField
 		$return = '<table class="pull-left">';
 		$return .= '<tr><td><input class="translated" size="30" type="text" value="' . JText::_('COM_LOCALISE_TEXT_TRANSLATION_TRANSLATED')
 					. '" readonly="readonly"/></td></tr>';
-		$return .= '<tr><td><input class="unchanged" size="30"  type="text" value="' . JText::_('COM_LOCALISE_TEXT_TRANSLATION_UNCHANGED')
-					. '" readonly="readonly"/></td></tr>';
 		$return .= '<tr><td><input class="untranslated" size="30"  type="text" value="' . JText::_('COM_LOCALISE_TEXT_TRANSLATION_UNTRANSLATED')
 					. '" readonly="readonly"/></td></tr>';
 		$return .= '<tr><td><input class="extra" size="30" type="text" value="' . JText::_('COM_LOCALISE_TEXT_TRANSLATION_NOTINREFERENCE')
