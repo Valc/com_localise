bd5b4c00ddfd05151abeccbb1051725706c9bee3
Valc <rafaelgomezcunill@gmail.com>
2015-02-22 13:39:58 +0100

    Db tables

--- component/admin/sql/install/mysql/install.sql
+++ component/admin/sql/install/mysql/install.sql
@@ -7,3 +7,31 @@ CREATE TABLE IF NOT EXISTS `#__localise` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_path` (`path`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__localise_specialuntranslatablekeys` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `client` varchar(255) NOT NULL,
+  `tag` varchar(6) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `untranslatablekeycase` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__localise_specialblockedkeys` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `client` varchar(255) NOT NULL,
+  `tag` varchar(6) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `blockedkeycase` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__localise_specialextrakeys` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `client` varchar(255) NOT NULL,
+  `tag` varchar(6) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `extrakeycase` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
--- component/admin/sql/install/mysql/uninstall.sql
+++ component/admin/sql/install/mysql/uninstall.sql
@@ -1,2 +1,5 @@
 DROP TABLE IF EXISTS `#__localise`;
+DROP TABLE IF EXISTS `#__localise_specialuntranslatablekeys`;
+DROP TABLE IF EXISTS `#__localise_specialblockedkeys`;
+DROP TABLE IF EXISTS `#__localise_specialextrakeys`;
 DELETE FROM `#__assets` WHERE `name` LIKE 'com_localise%';
